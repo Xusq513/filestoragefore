7e9416b2b4fe026a8ea7e445f5c008f2efefae61
xushaoqiang <xushaoqiang@ysstech.com>
2018-05-26 00:41:37 +0800

    调整了页面，增加了注释

--- js/config.js
+++ js/config.js
@@ -1,1 +1,7 @@
-var futrueUrl = "http://localhost:8080/"
+var futrueUrl = "http://localhost:8899/"
+
+
+const HTTP_POST = "post";
+const HTTP_GET = "get";
+const HTTP_PUT = "put";
+const HTTP_DELETE = "delete";
--- js/index.js
+++ js/index.js
@@ -1,21 +1,53 @@
 var vue = new Vue({
     el: '#container',
     data: {
+		/*
+		[
+			{
+				"id": 1,
+				"fileName": "生活文件夹",
+				"fileType": "dir",
+				"fileSize":"",
+				"lastModifyTime" : "2018-05-02 11:20:00"
+			}
+		]
+		*/
 		// 展示的文件对象
         fileItems: null,
+		/*
+		[
+			{
+				"id": 1,
+				"moduleName": "全部",
+				"iconClass": "glyphicon glyphicon-tasks",
+				"fileType": "all"
+			}
+		]
+		*/
 		// 左侧菜单对象
         moduleData: null,
+		/*
+		[
+			{
+				"name": fileName,
+				"id": id
+			}
+		]
+		*/
 		// 文件路径
         filePath: [],
-		// 视图类型
+		// 视图类型 all和other两种类型
         view: "all",
-		// 当前文件夹
+		// 当前文件夹 当前目录的主键
 		currentDirId:'',
-		// 右键当前操作文件的id
+		// 右键当前操作文件的主键
 		dbOperateFileId:''
     },
     methods: {
-        // 双击进入下一个子文件夹的事件
+		/**
+		 * 双击进入下一个子文件夹的事件
+		 * fileItem 点击的目录对象
+		 */
         showChildDir: function (fileItem) {
             // 如果不是文件夹直接返回
             if (fileItem.fileType != 'dir') {
@@ -26,24 +58,48 @@ var vue = new Vue({
                 var fileName = fileItem.fileName;
                 // 根据id查询子目录以及文件
 				index.queryFileByPid(id);
-                // 目录结构加入当前的目录
-                var path = {
-                    "name": fileName,
-                    "id": id
-                };
-                vue.filePath.push(path);
+				// 目录结构加入当前的目录
+				var path = {
+					"name": fileName,
+					"id": id
+				};
+				vue.filePath.push(path);
+				// 设置当前文件夹为跳转后的文件夹
 				this.currentDirId = id;
             }
 
         },
-        // 创建一个新的文件夹的事件或者重命名
+		/**
+		 * 保存文件信息，创建一个新的文件夹的事件或者重命名
+		 * fileItem 保存的文件对象 vue.fileItems数组内对象
+		 */
         saveDir: function (fileItem) {
 			var id = fileItem.id || '';
-			var cid = index.saveDirAjax(this.currentDirId,fileItem.fileName,id);
-            fileItem.type = 'label';
-			fileItem.id = cid;
+			var type = "";
+			if(id){
+				type = HTTP_PUT;
+			}else{
+				type = HTTP_POST;
+			}
+			index.saveDirAjax(type,this.currentDirId,fileItem.fileName,id,function(cid){
+				//alert(cid)
+				fileItem.type = 'label';
+				if(!id){
+					fileItem.id = cid;	
+				}else{
+					for(var fileItemIndex in vue.fileItems){
+						if(vue.fileItems[fileItemIndex].id == id){
+							Vue.set(vue.fileItems,fileItemIndex,vue.fileItems[fileItemIndex]);
+						}
+					}
+				}
+			});
+           
         },
-        // 点击全部时如果目录未加载过就先加载全部目录，并且切换到全部的视图下
+		/**
+		 * 点击左侧目录的切换事件，点击全部时如果目录未加载过就先加载全部目录，并且切换到全部的视图下
+		 * module 点击的目录对象 vue.moduleData的数组内对象
+		 */
         switchView: function (module) {
             if (module.fileType === 'all') {
                 this.view = 'all';
@@ -51,7 +107,10 @@ var vue = new Vue({
                 this.view = 'view';
             }
         },
-		// 点击目录的事件
+		/**
+		 * 点击导航目录的事件，切换到点击的目录
+		 * item 目录对象 vue.filePath的数组对象
+		 */
 		selectDir : function(item){
 			var filePathTemp = [];
 			for(let i=0;i<this.filePath.length;i++){
@@ -65,7 +124,11 @@ var vue = new Vue({
 			this.filePath = filePathTemp;
 			this.currentDirId = item.id;
 		},
-		// 计算文件大小 size的单位是Byte
+		// 
+		/**
+		 * 计算文件大小 转化为 G M K B
+		 * size 文件大小单位为Byte
+		 */
 		calFileSize : function(size){
 			if(!size){
 				return "-"
@@ -89,27 +152,29 @@ var vue = new Vue({
 });
 
 $(function () {
-    // 本地json文件模拟
-    $.getJSON('json/module.json', function (data) {
-        vue.moduleData = data;
-    });
+    // 左侧菜单的加载
+   	$.get(futrueUrl + 'module/get',function(data){
+   		vue.moduleData = data;
+   	});
     // 页面初始化，默认显示全部的条目，查询全部下的文件
 	$.get(futrueUrl + 'file/root',function(data){
-		 	vue.fileItems = data;
+		vue.fileItems = data;
+		var path = {
+			"name": "全部",
+			"id": ""
+		};
+		vue.filePath.push(path);
 	});
-		
-    var path = {
-        "name": "全部",
-        "id": ""
-    };
-    vue.filePath.push(path);
 	
+	// 右键菜单的事件
 	$('.context').contextmenu({
 		target:'#context-menu', 
+		// 右键之前触发的事件
 		before: function(e,context) {
 			// 右键之前先将操作的文件进行赋值
 			vue.dbOperateFileId = $(e.target).closest('tr').attr('id');
 		},
+		// 选择一个条目时触发的事件
 		onItem: function(context,e) {
 			var action = $(e.target).attr('action');
 			if(action){
@@ -136,32 +201,37 @@ $(function () {
 				}
 			}
 		}
-	})
-
-    // 	$.get(futrueUrl + 'module/get',function(data){
-    // 		vue.moduleData = data;
-    // 	});
+	});
 });
 
 index = {
-	// 点击创建文件夹
-	createDir : function (pid) {
+	/**
+	 * 点击创建文件夹
+	 * TODO 这里的“新建文件夹”应该判断当前文件夹是否有新建文件夹，有的话成为“新建文件夹1”，以此类推
+	 */
+	createDir : function () {
 	    var newDir = {
 	        "id": '',
 	        "fileName": "新建文件夹",
 	        "fileType": "dir",
 	        "fileSize": "",
-	        "lastModifyTime": "2018-05-05 11:20:00",
+	        "lastModifyTime": "",
 	        "type": "input"
 	    }
 	    vue.fileItems.unshift(newDir);
 	},
-	// 屏蔽原生的右键事件
+	/**
+	 * 屏蔽原生的右键事件
+	 */
 	doNothing : function (){  
 	        window.event.returnValue=false;  
 	        return false;  
 	},
-	// 根据fileItem的id获得fileItem对象
+	/**
+	 * 根据文件对象的id在文件夹数组对象中获得fileItem对象
+	 * id 要获得文件夹对象的主键
+	 * fileItems 文件对象数组，指vue.fileItems
+	 */
 	getFileItemById : function(id,fileItems){
 		for(var fileItemIndex in fileItems){
 			if(fileItems[fileItemIndex] == id){
@@ -169,13 +239,31 @@ index = {
 			}
 		}
 	},
-	// 提交文件夹的名称以及父节点到后台，返回当前节点的id
-	saveDirAjax : function(pid,fileName,id){
+	/**
+	* 根据文件对象的id在文件夹数组对象中获得索引值
+	* id 要获得文件夹对象的主键
+	* fileItems 文件对象数组，指vue.fileItems
+	*/
+	getIndexById : function(id,fileItems){
+		for(var fileItemIndex in fileItems){
+			if(fileItems[fileItemIndex] == id){
+				return fileItemIndex;
+			}
+		}
+	},
+	/**
+	 * 提交文件夹的名称以及父节点到后台，返回当前节点的id
+	 * type 两个值 post 发出post请求新增,put 发出put请求更新
+	 * pid 父目录的主键
+	 * fileName 目录的名称
+	 * id 目录的主键
+	 * callback 回调函数
+	 */
+	saveDirAjax : function(type,pid,fileName,id,callback){
 		var obj ={"pid":pid,"fileName":fileName,"id":id};
 		var targetUrl = futrueUrl + 'file';     
-		var newId = "";
 		$.ajax({ 
-			type:'post',  
+			type:type,  
 			url:targetUrl, 
 			cache: false,
 			data:obj, 
@@ -184,25 +272,31 @@ index = {
 			success:function(response){  
 			    if(response.success){
 					var data = response.data;
-					newId = data.id;
+					callback(data.id);
 				}else{
 					alert(response.message)
 				}
-			
 			},
 			error:function(){
 				alert("error");
 			}
 		});
-		return newId;
 	},
 	deleteFileAjax : function(){
 		
 	},
-	// 根据文件夹的ID查询文件夹下文件内容并且展示到页面上
-	queryFileByPid : function(pid){
+	/**
+	 * 根据文件夹的ID查询文件夹下文件内容并且展示到页面上
+	 * pid 父目录的主键
+	 * callback 回调函数
+	 */
+	queryFileByPid : function(pid,callback){
+		if(!pid){
+			pid = 'root';
+		}
 		$.get(futrueUrl + 'file/' + pid ,function(data){
-				vue.fileItems = data;
+			vue.fileItems = data;
+			callback();
 		});
 	}
 	
